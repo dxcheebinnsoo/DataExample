85f80084007a5ad1d4a0b098386bc6e2db6f6530
MICHAEL ROBINSON <CheeBinn Soo>
2020-11-11 17:18:25 +0000

    initilisation

---
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -0,0 +1,10 @@
+import { LightningElement, track, wire } from 'lwc';
+
+export default class TestComponent extends LightningElement {
+    @track
+    screenText = 'Hi There';
+
+    handleClick(){
+        this.screenText = 'Well Done!';
+    }
+}
