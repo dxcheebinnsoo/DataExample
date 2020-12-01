aed904c730ee89d7775b12dc06e387ceb68d8382
CheeBinnSoo <cheebinnsoo@gmail.com>
2020-12-01 14:11:12 +0000

    latest changes

---
+++ DataExampleNew/force-app/main/default/lwc/editRecord1/editRecord1.js
@@ -0,0 +1,5 @@
+import { api, LightningElement } from 'lwc';
+
+export default class EditRecord1 extends LightningElement {
+    @api recordId;
+}
---
+++ DataExampleNew/force-app/main/default/lwc/fetchContactViaApex/fetchContactViaApex.js
@@ -0,0 +1,16 @@
+import { LightningElement,wire } from 'lwc';
+import getAllContacts from '@salesforce/apex/ContactManager.getContact';
+
+export default class FetchContactViaApex extends LightningElement {
+    @wire(getAllContacts) contacts;
+
+    get responseReceived()
+    {
+        if(this.contacts)
+        {
+            return true;
+        }
+        return false;
+    } 
+
+}
